e702914dcc1e0b8c5ede1ea298c6537937f5d2a4
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-08 22:39:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const para = document.getElementById("timer");
+const currDate = new Date();
+const hr = currDate.getHours() > 12 ? currDate.getHours() - 12: currDate.getHours();
+const dayNight = currDate.getHours() > 12 ? 'PM': 'AM';
+para.innerText = `${currDate.getDate()}/${currDate.getMonth()}/${currDate.getFullYear()}, ${hr}:${currDate.getMinutes()}:${currDate.getSeconds()} ${dayNight}`;
